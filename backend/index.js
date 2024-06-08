e495408502a256c00ce0024d40533bd241ba39cf
ABHISHEK SHARMA <101241491+AbhishekSharma55@users.noreply.github.com>
2024-06-08 14:55:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://stack-overflow-null-class.vercel.app"],
+    origin: ["https://stack-overflow-null-class-frontend.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
